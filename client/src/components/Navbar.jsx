a01c7004a18d43eff318155de7de5d352b8bd6f4
Kenffy <voufomarius@yahoo.fr>
2023-12-08 10:17:07 +0100

    Implememt navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,29 +1,43 @@
 import "../assets/styles/components/navbar.css";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Logo from "../assets/images/cwd-logo.png";
+import { MdMenu } from "react-icons/md";
+import { MdClose } from "react-icons/md";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="navbar">
       <div className="container nav-wrapper">
         <div className="logo">
           <Link to="/">
-            <h2>CodeWithDavs</h2>
+            <img src={Logo} alt="cwd logo" />
           </Link>
         </div>
-        <div className="nav-links">
-          <Link to="/courses">
+        <div className={open ? "nav-links active" : "nav-links"}>
+          <Link to="/" onClick={() => setOpen(false)}>
+            <span>Home</span>
+          </Link>
+          <Link to="/courses" onClick={() => setOpen(false)}>
             <span>Courses</span>
           </Link>
-          <Link to="#topics">
+          <Link to="#topics" onClick={() => setOpen(false)}>
             <span>Topics</span>
           </Link>
-          <Link to="#contact">
+          <Link to="#contact" onClick={() => setOpen(false)}>
             <span>Contact</span>
           </Link>
-          <Link to="/login">
-            <span>Login</span>
+          <Link to="/login" onClick={() => setOpen(false)}>
+            <span className="login-btn">Login</span>
           </Link>
+        </div>
+        <div className="menu-btn" onClick={() => setOpen((prev) => !prev)}>
+          {open ? (
+            <MdClose className="menu-icon" />
+          ) : (
+            <MdMenu className="menu-icon" />
+          )}
         </div>
       </div>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,30 +4,30 @@ import axios from "axios";
 const BaseUrl = "https://localhost:5004/api/products";
 
 export default function Home() {
-  const [courses, setCoures] = useState([]);
-  useEffect(() => {
-    const loadCoursesAsync = async () => {
-      try {
-        const res = await axios.get(BaseUrl);
-        if (res) {
-          setCoures(res.data.result);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    loadCoursesAsync();
-  }, []);
+  // const [courses, setCoures] = useState([]);
+  // useEffect(() => {
+  //   const loadCoursesAsync = async () => {
+  //     try {
+  //       const res = await axios.get(BaseUrl);
+  //       if (res) {
+  //         setCoures(res.data.result);
+  //       }
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   loadCoursesAsync();
+  // }, []);
 
-  console.log(courses);
+  // console.log(courses);
   return (
     <div className="home">
       <section id="header">
-        <div className="s-header">Header</div>
+        <div className="s-header container">Header</div>
       </section>
 
       <section id="greeting">
-        <div className="s-greeting">
+        <div className="container s-greeting">
           <h2>Hello Welcome! I am Davy</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
@@ -39,7 +39,7 @@ export default function Home() {
       </section>
 
       <section id="topics">
-        <div className="s-topics">
+        <div className="container s-topics">
           <h4>Topics</h4>
           <ul>
             <li>Basics</li>
@@ -51,15 +51,15 @@ export default function Home() {
       </section>
 
       <section id="coures">
-        <div className="top-courses">Coures</div>
+        <div className="container top-courses">Coures</div>
       </section>
 
       <section id="testimonials">
-        <div className="s-feedbacks">Feedbacks</div>
+        <div className="container s-feedbacks">Feedbacks</div>
       </section>
 
       <section id="Contact">
-        <div className="s-contact">Contact</div>
+        <div className="container s-contact">Contact</div>
       </section>
     </div>
   );
